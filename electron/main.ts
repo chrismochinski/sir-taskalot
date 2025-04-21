9604cbd6c209713d555ee848ebf078120c086f36
Chris Mochinski <cmochinski@gmail.com>
2025-04-21 17:57:14 -0500

    Initial commit - basic wiring and app foundation

---
+++ electron/main.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
