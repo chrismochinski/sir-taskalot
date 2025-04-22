a77aa1c0bfbf1771625bfb896296e3974ef558d4
Chris Mochinski <cmochinski@gmail.com>
2025-04-22 14:43:41 -0500

    clean up indeximport

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import { NewTicketForm } from './NewTicketForm';
-import './index.css';
+// import './index.css'; // deletelater after confirming overflow
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
